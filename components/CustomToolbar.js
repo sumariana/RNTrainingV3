2b5ad63d98fc62a8bf4e7e9afa87848d8a826a8d
sumariana <sumarianakadek@gmail.com>
2021-04-22 19:18:31 +0800

    adas

---
+++ components/CustomToolbar.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, View,Platform } from 'react-native';
+
+const headerHeight = Platform.OS === "android" ? 56 : 46;
+const customToolbar = props=>{
+    return (
+        <View style={{...styles.container,...props.containerStyle}}>
+            <View 
+                style={{...styles.customToolbar,...props.style}}>
+                {props.children}
+            </View>
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container:{
+        overflow:'hidden',
+        paddingBottom:5
+    },
+    customToolbar: {
+        height: headerHeight,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        shadowColor: '#000',
+        shadowOpacity:  0.4,
+        shadowRadius: 3,
+        elevation: 10,
+        shadowOffset: { width: 1, height: 1 },
+    },
+})
+
+export default customToolbar;
--- screens/MyPageScreen.js
+++ screens/MyPageScreen.js
@@ -5,27 +5,27 @@ import SpacerTop from '../components/SpacerTop';
 import FocusAwareStatusBar from '../components/FocusAwareStatusBar';
 import {Icon} from 'react-native-elements';
 import Colors from '../constants/Colors';
+import CustomToolbar from '../components/CustomToolbar'
+import CustomInput from '../components/CustomInput';
 
-const headerHeight = Platform.OS === "android" ? 56 : 46;
 const MyPageScreen = props =>{
     return (
         <SafeAreaView style={styles.container}>
             <FocusAwareStatusBar
                 barStyle='dark-content'
                 backgroundColor='white' />
-                <View style={{overflow:'hidden',paddingBottom:5}}>
-                    <View 
-                        style={styles.customToolbar}>
-                        <Text style={styles.title}>TrainingApps</Text>
-                        <TouchableOpacity
-                            style={styles.logoutContainer}
-                            onPress={() => {}}>
-                            <Text style={styles.logoutText}>Keluar</Text>
-                        </TouchableOpacity>
-                    </View>
-                </View>
-            <ScrollView contentContainerStyle={{flexGrow:1}}>
+                <CustomToolbar>
+                    <Text style={styles.title}>TrainingApps</Text>
+                    <TouchableOpacity
+                        style={styles.logoutContainer}
+                        onPress={() => {}}>
+                        <Text style={styles.logoutText}>Keluar</Text>
+                    </TouchableOpacity>
+                </CustomToolbar>
+            <ScrollView contentContainerStyle={styles.scrollView}>
+                <View style={{flexDirection:''}}>
 
+                </View>
             </ScrollView>
         </SafeAreaView>
     );
@@ -42,16 +42,8 @@ const styles = StyleSheet.create({
         flex: 1,
         marginTop: Platform.OS == 'ios' ? StatusBar.currentHeight : 0
     },
-    customToolbar: {
-        height: headerHeight,
-        backgroundColor: 'white',
-        justifyContent: 'center',
-        alignItems: 'center',
-        shadowColor: '#000',
-        shadowOpacity:  0.4,
-        shadowRadius: 3,
-        elevation: 10,
-        shadowOffset: { width: 1, height: 1 },
+    scrollView:{
+        flexGrow:1,padding:20
     },
     title: {
         fontFamily: 'rubik-bold',
